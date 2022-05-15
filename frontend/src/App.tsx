ce9cf9d1cc8cb7b06c65539720fdfbceb5bbeea7
JCastro456 <jackelinecastrocardoso@hotmail.com>
2022-05-15 17:28:40 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
+import NavBar from "./components/Navbar";
 
 function App() {
-  return (
-    <h1>Página principal</h1>
+  return (  
+    <NavBar  />
   );
 }
-
-export default App;
+export default App
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './style.css';
+
+function Navbar() {
+    return (
+      <header>
+          <nav className="container">
+            <div className="dsmovie-nav-content">
+               <h1>DSMovie</h1>
+               <a href="https://github.com/JCastro456" target="_blank" rel="noreferrer">
+                  <div className="dsmovie-contact-container">
+                   <GithubIcon />
+                    <p className="dsmovie-contact-link">/JCastro456</p>
+                  </div>
+               </a>
+            </div>
+          </nav>
+       </header>
+    );
+}
+export default Navbar;
