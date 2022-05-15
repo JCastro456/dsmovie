21a83e03779b2028f127727f1a917748d85e6d31
JCastro456 <jackelinecastrocardoso@hotmail.com>
2022-05-15 19:50:51 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,33 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard/index.";
+
 
 function Listing() {
     return (
+        <>
         <Pagination />
+        
+        <div className="container">        
+           <div className="row">
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                  <MovieCard />
+              </div>
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                  <MovieCard />
+              </div>
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                  <MovieCard />
+              </div>
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                  <MovieCard />
+              </div>
+              <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                  <MovieCard />
+              </div>
+          </div>
+        </div>
+        
+        </>
     );
 }
 export default Listing;
